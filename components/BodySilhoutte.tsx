2aeb50dcb80061ca1ada5df1c524d18b688b9c92
Lois Narh <loisnarh3@gmail.com>
2026-07-28 21:45:39 +0000

    Updated Body Silhoutte

--- components/BodySilhoutte.tsx
+++ components/BodySilhoutte.tsx
@@ -1,86 +1,115 @@
 import React from "react";
 import { View, StyleSheet, Dimensions } from "react-native";
-import Svg, { Path, Ellipse, G } from "react-native-svg";
+import Svg, { Path, Circle, Ellipse, G } from "react-native-svg";
 
 const { width, height } = Dimensions.get("window");
+
+export type BodyGender = "male" | "female";
 
 interface BodySilhouetteProps {
   opacity?: number;
   color?: string;
   view?: "front" | "side";
+  gender?: BodyGender;
+  size?: "full" | "small";
 }
 
 export default function BodySilhouette({
   opacity = 0.35,
   color = "#b8f54a",
   view = "front",
+  gender = "male",
+  size = "full",
 }: BodySilhouetteProps) {
-  const svgHeight = height * 0.7;
-  const svgWidth = width * 0.6;
-  const cx = svgWidth / 2;
+  const svgHeight = size === "small" ? 90 : height * 0.7;
+  const svgWidth = size === "small" ? 40 : width * 0.6;
 
   return (
     <View style={[styles.container, { width: svgWidth, height: svgHeight }]}>
       <Svg
         width={svgWidth}
         height={svgHeight}
-        viewBox="0 0 200 500"
+        viewBox="0 0 160 400"
         opacity={opacity}
       >
-        {view === "front" ? (
-          <G fill={color}>
-            {/* Head */}
-            <Ellipse cx="100" cy="40" rx="28" ry="35" />
-            {/* Neck */}
-            <Path d="M88 72 Q100 80 112 72 L115 95 Q100 100 85 95 Z" />
-            {/* Shoulders */}
-            <Path d="M55 95 Q70 88 85 95 L85 140 Q70 145 55 135 Z" />
-            <Path d="M115 95 Q130 88 145 95 L145 135 Q130 145 115 140 Z" />
-            {/* Torso */}
-            <Path d="M85 95 Q100 100 115 95 L120 200 Q100 210 80 200 Z" />
-            {/* Left arm */}
-            <Path d="M55 95 Q45 120 42 160 Q40 190 44 220 Q50 225 58 220 Q62 190 65 160 Q68 130 65 110 Z" />
-            {/* Right arm */}
-            <Path d="M145 95 Q155 120 158 160 Q160 190 156 220 Q150 225 142 220 Q138 190 135 160 Q132 130 135 110 Z" />
-            {/* Left hand */}
-            <Ellipse cx="51" cy="230" rx="10" ry="14" />
-            {/* Right hand */}
-            <Ellipse cx="149" cy="230" rx="10" ry="14" />
-            {/* Hips */}
-            <Path d="M80 200 Q100 210 120 200 L125 245 Q100 255 75 245 Z" />
-            {/* Left leg */}
-            <Path d="M75 245 Q68 280 66 330 Q64 370 66 410 Q72 415 80 412 Q84 370 86 330 Q88 285 88 250 Z" />
-            {/* Right leg */}
-            <Path d="M125 245 Q132 280 134 330 Q136 370 134 410 Q128 415 120 412 Q116 370 114 330 Q112 285 112 250 Z" />
-            {/* Left foot */}
-            <Ellipse cx="73" cy="420" rx="14" ry="8" />
-            {/* Right foot */}
-            <Ellipse cx="127" cy="420" rx="14" ry="8" />
-          </G>
-        ) : (
-          <G fill={color}>
-            {/* Head side */}
-            <Ellipse cx="95" cy="40" rx="25" ry="35" />
-            {/* Neck */}
-            <Path d="M85 72 Q95 80 105 72 L108 95 Q95 100 82 95 Z" />
-            {/* Torso side */}
-            <Path d="M75 95 Q110 100 115 95 L118 200 Q105 215 72 205 Z" />
-            {/* Arm side */}
-            <Path d="M112 100 Q125 130 128 170 Q130 200 126 225 Q120 228 115 225 Q112 200 110 170 Q108 135 108 105 Z" />
-            {/* Hand */}
-            <Ellipse cx="121" cy="233" rx="9" ry="13" />
-            {/* Hips side */}
-            <Path d="M72 205 Q105 215 118 200 L120 248 Q100 260 70 250 Z" />
-            {/* Legs side */}
-            <Path d="M70 250 Q68 290 67 335 Q66 375 68 415 Q74 418 82 415 Q84 375 85 335 Q86 290 85 252 Z" />
-            <Path d="M100 248 Q102 288 103 333 Q104 373 102 413 Q108 416 116 413 Q118 373 117 333 Q116 288 114 250 Z" />
-            {/* Feet */}
-            <Ellipse cx="75" cy="422" rx="16" ry="8" />
-            <Ellipse cx="109" cy="421" rx="13" ry="7" />
-          </G>
-        )}
+        {view === "front"
+          ? gender === "female"
+            ? <FemaleFront color={color} />
+            : <MaleFront color={color} />
+          : gender === "female"
+            ? <FemaleSide color={color} />
+            : <MaleSide color={color} />}
       </Svg>
     </View>
+  );
+}
+
+function MaleFront({ color }: { color: string }) {
+  return (
+    <G fill={color}>
+      <Circle cx="80" cy="34" r="22" />
+      <Path d="M70,54 Q80,60 90,54 L92,72 Q80,76 68,72 Z" />
+      <Path d="M42,76 C36,100 44,130 48,160 C46,180 46,190 46,196 L114,196 C114,190 114,180 112,160 C116,130 124,100 118,76 Q80,66 42,76 Z" />
+      <Path d="M40,80 C26,110 22,150 26,190 C28,210 32,225 38,234 L48,234 C42,220 44,200 46,170 C48,140 44,110 48,82 Z" />
+      <Path d="M120,80 C134,110 138,150 134,190 C132,210 128,225 122,234 L112,234 C118,220 116,200 114,170 C112,140 116,110 112,82 Z" />
+      <Circle cx="44" cy="240" r="9" />
+      <Circle cx="116" cy="240" r="9" />
+      <Path d="M46,196 C42,230 40,280 42,330 C43,355 44,370 46,382 L70,382 C72,370 74,355 75,330 C76,280 74,230 76,198 Z" />
+      <Path d="M114,196 C118,230 120,280 118,330 C117,355 116,370 114,382 L90,382 C88,370 86,355 85,330 C84,280 86,230 84,198 Z" />
+      <Ellipse cx="50" cy="390" rx="17" ry="8" />
+      <Ellipse cx="110" cy="390" rx="17" ry="8" />
+    </G>
+  );
+}
+
+function FemaleFront({ color }: { color: string }) {
+  return (
+    <G fill={color}>
+      <Circle cx="80" cy="33" r="21" />
+      <Path d="M52,48 C48,60 47,70 50,78 L44,80 C40,64 41,50 48,42 Z" />
+      <Path d="M108,48 C112,60 113,70 110,78 L116,80 C120,64 119,50 112,42 Z" />
+      <Path d="M71,52 Q80,58 89,52 L91,68 Q80,72 69,68 Z" />
+      <Path d="M50,74 C44,92 50,110 58,126 C52,140 48,160 50,178 C48,188 48,193 48,196 L112,196 C112,193 112,188 110,178 C112,160 108,140 102,126 C110,110 116,92 110,74 Q80,64 50,74 Z" />
+      <Path d="M48,78 C36,102 33,132 37,160 C39,176 43,188 48,196 L56,196 C52,184 50,170 49,150 C48,128 51,104 56,84 Z" />
+      <Path d="M112,78 C124,102 127,132 123,160 C121,176 117,188 112,196 L104,196 C108,184 110,170 111,150 C112,128 109,104 104,84 Z" />
+      <Circle cx="41" cy="200" r="8" />
+      <Circle cx="119" cy="200" r="8" />
+      <Path d="M44,196 C40,228 40,270 44,315 C46,340 47,360 48,378 L68,378 C70,360 72,340 73,315 C75,270 74,228 78,198 Z" />
+      <Path d="M116,196 C120,228 120,270 116,315 C114,340 113,360 112,378 L92,378 C90,360 88,340 87,315 C85,270 86,228 82,198 Z" />
+      <Ellipse cx="52" cy="386" rx="16" ry="7" />
+      <Ellipse cx="108" cy="386" rx="16" ry="7" />
+    </G>
+  );
+}
+
+function MaleSide({ color }: { color: string }) {
+  return (
+    <G fill={color}>
+      <Circle cx="88" cy="34" r="21" />
+      <Path d="M108,28 Q118,32 109,40 Z" />
+      <Path d="M78,52 Q88,58 98,52 L100,70 Q88,74 76,70 Z" />
+      <Path d="M76,74 C68,100 66,130 70,158 C72,175 76,188 82,196 L100,196 C108,185 114,165 116,140 C118,115 114,90 108,74 Q88,64 76,74 Z" />
+      <Path d="M96,80 C90,110 88,150 92,190 C93,205 95,218 98,228 L108,228 C104,215 103,200 104,175 C106,145 104,110 110,82 Z" />
+      <Circle cx="102" cy="234" r="9" />
+      <Path d="M72,196 C68,230 66,280 70,330 C72,355 74,372 78,384 L100,384 C102,372 103,355 104,330 C106,280 104,230 100,198 Z" />
+      <Path d="M66,384 Q64,398 76,398 L120,396 Q132,394 128,386 L100,384 Z" />
+    </G>
+  );
+}
+
+function FemaleSide({ color }: { color: string }) {
+  return (
+    <G fill={color}>
+      <Circle cx="88" cy="33" r="20" />
+      <Path d="M68,20 C58,26 54,40 58,54 C60,62 64,68 70,72 L66,50 C64,40 66,28 74,20 Z" />
+      <Path d="M106,27 Q115,31 107,38 Z" />
+      <Path d="M78,50 Q88,56 98,50 L100,66 Q88,70 76,66 Z" />
+      <Path d="M74,74 C68,92 68,108 70,124 C68,145 64,160 66,178 C65,188 68,194 74,196 L108,196 C114,188 116,175 114,158 C118,140 122,115 116,95 C112,80 108,72 104,74 Q88,64 74,74 Z" />
+      <Path d="M92,80 C88,108 87,145 90,182 C91,196 93,208 96,218 L104,218 C101,206 100,193 101,170 C102,142 100,110 104,82 Z" />
+      <Circle cx="98" cy="223" r="8" />
+      <Path d="M72,196 C69,225 68,270 71,315 C73,340 75,358 78,372 L98,372 C100,358 101,340 102,315 C104,270 102,225 100,198 Z" />
+      <Path d="M66,372 Q64,384 74,384 L114,382 Q124,380 120,373 L98,372 Z" />
+    </G>
   );
 }
 
